6d689a587c5ca0ab5a15682365804c7a3218457f
ryanmelnyk <schmelnyk@gmail.com>
2024-04-11 08:59:57 -0700

    bugfix in combine_csvs.py

--- bin/combine_csvs.py
+++ bin/combine_csvs.py
@@ -2,21 +2,21 @@
 
 import os
 
-dfs = []
+files = [
+    x for x in os.listdir(".") if x.endswith(".csv")
+]
 
 o = open("genome_stats.csv", 'w')
-
 header = False
-for f in os.listdir("."):
-    if f.endswith(".csv"):
-        for line in open(f, 'r'):
-            if line.startswith("accession,"):
-                if header:
-                    continue
-                else:
-                    o.write(line)
-                    header=True
+for f in files:
+    for line in open(f, 'r'):
+        if line.startswith("accession,"):
+            if header:
+                continue
             else:
                 o.write(line)
+                header = True
+        else:
+            o.write(line)
 
 o.close()
