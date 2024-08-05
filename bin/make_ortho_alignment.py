224bb762c5d184a40701d51fc3914d4fbbd0ce41
ryanmelnyk <schmelnyk@gmail.com>
2024-08-05 08:09:37 -0700

    refactor to remove slow string concatenation code

--- bin/make_ortho_alignment.py
+++ bin/make_ortho_alignment.py
@@ -9,7 +9,7 @@ import string
 strains = [line.rstrip() for line in open(sys.argv[1], 'r')]
 orthos = [line.rstrip() for line in open("ortholog_groups.txt", 'r')]
 
-align_data = {k: "" for k in strains}
+align_data = {k: {l: None for l in orthos} for k in strains}
 align_genes = {k: {l: None for l in orthos} for k in strains} 
 
 print(f"Creating master alignment...\n")
@@ -37,33 +37,33 @@ for o in orthos:
                 fields = vals[0].split("|")
                 if align_genes[fields[1]][o] == None:
                     align_genes[fields[1]][o] = fields[0]
-                    align_data[fields[1]] += vals[1].translate(translation)
+                    align_data[fields[1]][o] = vals[1].translate(translation)
                 elif fields[0] == align_genes[fields[1]][o]:
-                    align_data[fields[1]] += vals[1].translate(translation)
-                elif fields[0] != align_genes[fields[1]][o]:
-                    pass
+                    align_data[fields[1]][o] += vals[1].translate(translation)
                 else:
                     pass
 
     for strain in strains:
         if align_genes[strain][o] == None:
-            align_data[strain] += "-" * length
-        if len("".join(align_data[strain])) != total_leng:
-            print(strain, "is short!")
-            print(total_leng, len("".join(align_data[strain])))
-            print(align_data[strain])
-            print(o)
-            sys.exit()
+            align_data[strain][o] = "-" * length
     if count % 10 == 0:
-        print(f"{count} orthologs processed...")
+        print(f"{count} Stockholm files processed...")
     else:
         pass
-print("Done!")
+
+print(f"Done with {count} ortholog alignments!")
 print("Writing alignment...")
 out = open("master_alignment.faa", 'w')
 
-for a in align_data:
-    out.write(f">{a}\n")
-    alignment = align_data[a].upper().replace(".","-")
-    out.write(f"{alignment}\n")
+count = 0
+for strain in strains:
+    count += 1
+    out.write(f">{strain}\n")
+    for o in orthos:
+        alignment = align_data[strain][o].upper().replace(".","-")
+        out.write(f"{alignment}")
+    out.write("\n")
+    if count % 100 == 0:
+        print(f"{count} strain alignments written...") 
 out.close()
+print(f"Done writing {count} strain alignments!")
